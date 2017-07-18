d6efef5015d8a43e8226b97bf2737a5fd19a867b
dannycr1 <danny.croitoru@gmail.com>
2017-07-18 20:43:58 +0300

    Create an intial page structure: simple header ( + 2 forms), navigator, body with content, footer load initial Json of Data.

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-var passportApp = angular.module('passportApp', []);
+var bubleApp = angular.module('bubleApp', []);
 
-passportApp.controller('PassportListCtrl', function($scope, $http) {
-  $http.get('data.json').then(function(res) {
-      $scope.data = res.data;
+bubleApp.controller('BubleListCtrl', function ($scope, $http) {
+    function Buble(data) {
+        this.date = data.date;
+        this.time = data.time;
+        this.user = data.user;
+        this.content = data.content;
+        this.isClicked = false;
+    }
+
+    $scope.bubles = [];
+
+    $http.get("data.json").then(function successCallback(response) {
+        for (var i = 0; i < response.data.length; i++) {
+            $scope.bubles.push(new Buble(response.data[i]));
+        }
     });
 });
+
