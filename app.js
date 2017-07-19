e7b95f5ff592bfc30ad0320077214513071a6c48
dannycr1 <danny.croitoru@gmail.com>
2017-07-19 22:23:06 +0300

    Working with chrom Server

--- app.js
+++ app.js
@@ -4,21 +4,23 @@ bubleApp.config(function($httpProvider){
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
 
+
 bubleApp.controller('BubleListCtrl', function ($scope, $http) {
     function Buble(data) {
         this.date = data.date;
         this.time = data.time;
         this.user = data.user;
         this.content = data.content;
-        this.isClicked = false;
     }
 
     $scope.bubles = [];
 
-    $http.get("https://github.com/dannycr1/Final_project/blob/master/data.json").then(function successCallback(response) {
+    $http.get("http://127.0.0.1:8887/data.json").then(function successCallback(response) {
         for (var i = 0; i < response.data.length; i++) {
             $scope.bubles.push(new Buble(response.data[i]));
         }
     });
 });
+
+
 
